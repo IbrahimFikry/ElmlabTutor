328c13023ade67b69b4db8a9fc71fdaa749d056a
Ibrahim <baim008@gmail.com>
2018-06-12 14:23:39 +0800

    added login function

--- App.js
+++ App.js
@@ -27,37 +27,20 @@ var config = {
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      nameinput : 'hi!',
-    }
-  }
-
   _page1 = () => {
     this.props.navigation.navigate('Page1');
   }
   _page2 = () => {
     this.props.navigation.navigate('Page2');
   }
-
-  _addfirebase = () => {
-    firebase.database().ref('test/uniqueid1').set({
-        hello: 'hello',
-      })
+  _pagefirebase = () => {
+    this.props.navigation.navigate('PageFirebase');
   }
 
-    _addvaluetofirebase = () => {
-      firebase.database().ref('success').set({
-          email: this.state.nameinput,
-        })
-    }
-    _readfromfirebase = () => {
-      firebase.database().ref('success').on("value", function (snapshot) {
-        console.log(snapshot.val().email);
-      })
-    }
+  _pagelogin = () => {
+    this.props.navigation.navigate('PageLogin');
+  }
+
 
   render() {
 
@@ -66,21 +49,49 @@ class App extends React.Component {
 
       <Button onPress={this._page1} title="Go to page 1"/>
       <Button onPress={this._page2} title="Go to page 2"/>
-      <Button onPress={this._addfirebase} title="Add firebase"/>
+      <Button onPress={this._pagefirebase} title="Go to page firebase"/>
+      <Button onPress={this._pagelogin} title="Go to page login"/>
 
-      <Text>{this.state.nameinput}</Text>
-
-      <TextInput placeholder="Insert here"
-      value={this.state.nameinput} onChangeText={ (x) => {this.setState({ nameinput: x}) } } />
-
-      <Button onPress={this._addvaluetofirebase} title="Add the input to the database"/>
-      <Button onPress={this._readfromfirebase} title="Read"/>
+      <Text></Text>
 
       </View>
     );
   }
 }
 
+
+class PageLogin extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email:'',
+      password:'',
+    }
+  }
+
+  _loginnow = () => {
+    firebase.auth().signInWithEmailAndPassword(this.state.email,this.state.password)
+    .then( (userData) => {
+      console.log(userData.uid);
+    }).catch ( (error) => {
+      console.log(error);
+    })
+  }
+
+  render() {
+    return(
+      <View>
+      <Text>Login Page</Text>
+      <Text>Email</Text>
+      <TextInput value={this.state.email} onChangeText={ (x) => this.setState({email:x}) }/>
+      <Text>Password</Text>
+      <TextInput value={this.state.password} onChangeText= {(x) => this.setState({password:x})}/>
+      <Button onPress={this._loginnow} title="Login"/>
+      </View>
+    )
+  }
+}
 class Page1 extends React.Component {
   render() {
     return (
@@ -102,11 +113,57 @@ class Page2 extends React.Component {
     )
   }
 }
+class PageFirebase extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nameinput : 'hi!',
+      read: '',
+    }
+  }
+
+    _addfirebase = () => {
+      firebase.database().ref('test/uniqueid1').set({
+          hello: 'hello',
+        })
+    }
+
+      _addvaluetofirebase = () => {
+        firebase.database().ref('success').set({
+            email: this.state.nameinput,
+          })
+      }
+      _readfromfirebase = () => {
+        firebase.database().ref('success').on("value", function (snapshot) {
+          console.log(snapshot.val().email);
+          this.setState({ read: snapshot.val().email})
+        })
+      }
+  render() {
+    return (
+      <View>
+      <Text>This is firebase page example</Text>
+      <Button onPress={this._addfirebase} title="Add firebase"/>
+
+      <Text>{this.state.nameinput}</Text>
+
+      <TextInput placeholder="Insert here"
+      value={this.state.nameinput} onChangeText={ (x) => {this.setState({ nameinput: x}) } } />
+
+      <Button onPress={this._addvaluetofirebase} title="Add the input to the database"/>
+      <Button onPress={this._readfromfirebase} title="Read"/>
+      </View>
+    )
+  }
+}
 
 export default StackNavigator({
   Home : {screen: App,},
   Page1 : {screen: Page1,},
   Page2 : {screen: Page2,},
+  PageFirebase : {screen: PageFirebase,},
+  PageLogin : {screen: PageLogin,},
 })
 
 
@@ -116,7 +173,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingTop: Constants.statusBarHeight,
     //alignItems: 'center',
-    //justifyContent: 'center',
+    justifyContent: 'center',
   },
 
 
